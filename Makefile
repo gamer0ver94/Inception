ee8c9ac4353e62c0eb1c7ac864118c3b8a38807f
duarte <duarte.paulino94@hotmail.com>
2023-10-01 17:27:24 +0100

    changed variables on make file

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 all:
-		@sudo mkdir -p  volumes
-		@sudo mkdir -p volumes/db
-		@sudo mkdir -p volumes/wp
+		@sudo mkdir -p  /home/$(USER)/data
+		@sudo mkdir -p /home/$(USER)/data/db
+		@sudo mkdir -p /home/$(USER)/data/wp
 		@sudo docker-compose -f srcs/docker-compose.yml up -d --build
 up:
-		@sudo mkdir  volumes
-		@sudo mkdir volumes/db
-		@sudo mkdir volumes/wp
+		@sudo mkdir  /home/$(USER)/data
+		@sudo mkdir /home/$(USER)/data/db
+		@sudo mkdir /home/$(USER)/data/wp
 		@docker-compose -f srcs/docker-compose.yml up -d
 down:
 		@sudo docker-compose -f srcs/docker-compose.yml down
@@ -15,7 +15,7 @@ status:		@sudo docker ps
 
 
 clean:
-		@sudo rm -rf volumes
+		@sudo rm -rf /home/$(USER)/data
 		@sudo docker volume rm db wp
 fclean:clean
 		@sudo docker system prune -af
--- srcs/requirements/wordpress/tools/wp-conf.sh
+++ srcs/requirements/wordpress/tools/wp-conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sleep 10
 if [ ! -d /var/www/wordpress ]; then
 	mkdir -p /var/www/wordpress
 	chown -R www-data:www-data "/var/www/wordpress"
@@ -8,13 +8,13 @@ cd /var/www/wordpress
 if [ ! -f "wp-config.php" ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
-	wp cli update
 	mv wp-cli.phar /usr/local/bin/wp
+	wp cli update
 	wp core download --allow-root
 	wp core config --allow-root --dbhost=mariadb --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}"
 	wp core install --allow-root --path=/var/www/wordpress --url="https://192.168.1.24" --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
 fi
 mkdir -p /run/php/
 chown www-data:www-data /run/php/
-echo "Wordpress is now up"
 php-fpm7.4 -F
+echo "Wordpress service is now up!"
