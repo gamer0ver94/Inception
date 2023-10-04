0ea899808af6e6583fcbd6ccbca04da872ab9003
gamer0ver94 <duarte.paulino94@hotmail.com>
2023-10-04 07:27:37 -0400

    finished

--- Makefile
+++ Makefile
@@ -1,23 +1,20 @@
 all:
-		@sudo mkdir -p  /home/$(USER)/data
-		@sudo mkdir -p /home/$(USER)/data/db
-		@sudo mkdir -p /home/$(USER)/data/wp
-		@sudo docker-compose -f srcs/docker-compose.yml up -d --build
+		@mkdir -p /home/$(USER)/data/db
+		@mkdir -p /home/$(USER)/data/wp
+		@docker compose -f srcs/docker-compose.yml up -d
 up:
-		@sudo mkdir  /home/$(USER)/data
-		@sudo mkdir /home/$(USER)/data/db
-		@sudo mkdir /home/$(USER)/data/wp
-		@docker-compose -f srcs/docker-compose.yml up -d
+		@docker compose -f srcs/docker-compose.yml up -d
 down:
-		@sudo docker-compose -f srcs/docker-compose.yml down
+		@docker compose -f srcs/docker-compose.yml down
 
-status:		@sudo docker ps
-
+status:		@docker ps
 
 clean:
 		@sudo rm -rf /home/$(USER)/data
-		@sudo docker volume rm db wp
-fclean:clean
-		@sudo docker system prune -af
+		@docker volume rm db wp
+fclean:	clean
+		@docker system prune -af
+prune:
+		@docker system prune -af
 
-re:down clean all
+re:	down clean all
--- srcs/requirements/wordpress/tools/wp-conf.sh
+++ srcs/requirements/wordpress/tools/wp-conf.sh
@@ -9,12 +9,16 @@ if [ ! -f "wp-config.php" ]; then
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
 	mv wp-cli.phar /usr/local/bin/wp
-	wp cli update
 	wp core download --allow-root
+	echo "Configuring admin"
 	wp core config --allow-root --dbhost=mariadb --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}"
-	wp core install --allow-root --path=/var/www/wordpress --url="https://192.168.1.24" --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL}
+	wp core install --allow-root --path=/var/www/wordpress --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --skip-email
+	echo "Configuring user"
+	wp user create --allow-root --path=/var/www/wordpress ${USER_NAME} ${USER_MAIL} --role=author --user_pass=${USER_PASSWORD}
 fi
+echo "creating folder"
 mkdir -p /run/php/
 chown www-data:www-data /run/php/
-php-fpm7.4 -F
 echo "Wordpress service is now up!"
+exec php-fpm7.4 -F
+echo "DONE"
